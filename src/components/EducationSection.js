82cc87c396e0747961cd968c5b04330fc26ddb0e
Rostislav Grishin <grishin.rost@gmail.com>
2026-02-25 00:33:16 -0700

    Fix unused imports for Vercel build

--- src/components/EducationSection.js
+++ src/components/EducationSection.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaGraduationCap, FaCertificate, FaBookOpen } from "react-icons/fa";
+import { FaGraduationCap } from "react-icons/fa";
 
 const EducationSection = () => {
     return (
